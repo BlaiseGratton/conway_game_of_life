f95a3c80ce8ccf06ff2e817a5129405c493c34f5
BlaiseGratton <blaisegratton@gmail.com>
2014-10-28 15:42:51 -0500

    Functioning Conway's Game of Life; random or templates

--- main.js
+++ main.js
@@ -9,13 +9,10 @@ function matrixCreator(x, y) {
   return matrix;
 }
 
-function newCycle(matrix){
+function generateNewCycle(matrix){
   console.log(matrix);
-  //rule 1
-  //rule 2
-  //rule 3
-  //rule 4
   var $table = document.querySelector("table");
+  $table.innerHTML = '';
   matrix.forEach(function(row){
     // var row = matrix[i];
     $tr = document.createElement("tr");
@@ -25,10 +22,12 @@ function newCycle(matrix){
 
       if (cell === 1) {
         $td.classList.add("alive");
-      } else {
-        $td.classList.remove("alive");  
+        $td.classList.remove("dead");
+      } else if (cell === 0){
+        $td.classList.remove("alive");
+        $td.classList.add("dead");  
       }
-
+      $td.textContent = cell;
       $tr.appendChild($td);
 
     });
@@ -36,10 +35,112 @@ function newCycle(matrix){
   });
 }
 
-var matrix = matrixCreator(3, 3);
+function livingNeighborCount(x, y){
+  console.log(x, y);
+  var neighborCounter = [];
+  if ( x-1 >= 0 && y-1 >= 0 && matrix[x-1][y-1] === 1){
+    neighborCounter.push(1);
+  }  
+  if ( x-1 >= 0 && matrix[x-1][y] === 1){
+    neighborCounter.push(1);
+  } 
+  if ( x-1 >= 0 && y+1 <= matrix[x].length && matrix[x-1][y+1] === 1){
+    neighborCounter.push(1);
+  } 
+  if ( y+1 <= matrix[x].length && matrix[x][y+1] === 1){
+    neighborCounter.push(1);
+  } 
+  if ( x+1 < matrix.length && y+1 < matrix[x].length && matrix[x+1][y+1] === 1){
+    neighborCounter.push(1);
+  } 
+  if ( x+1 < matrix.length && matrix[x+1][y] === 1){
+    neighborCounter.push(1);
+  } 
+  if ( x+1 < matrix.length && y-1 >= 0 && matrix[x+1][y-1] === 1){
+    neighborCounter.push(1);
+  } 
+  if ( y-1 >= 0 && matrix[x][y-1] === 1){
+    neighborCounter.push(1);
+  }
+  return neighborCounter.length;
+}
+
+
+function calculateNextState(currentState) {
+  var nextState = [];
+  currentState.forEach(function(currentRow, x) {
+    var nextRow = [];
+    currentRow.forEach(function(currentCell, y){
+      var nextCellState; 
+      var neighborCounter = livingNeighborCount(x, y);
+      console.log(neighborCounter);
+      var currentCell = currentState[x][y];
+      if (currentCell === 1 && neighborCounter <= 1){
+        nextCellState = 0;
+      } 
+      if (currentCell === 1 && (neighborCounter === 2 || neighborCounter === 3)){
+        nextCellState = 1;
+      } 
+      if (currentCell === 1 && neighborCounter >= 4){
+        nextCellState = 0;
+      } 
+      if (currentCell === 0 && neighborCounter === 3){
+        nextCellState = 1;
+      }
+      if (currentCell === 0 && (neighborCounter < 3 || neighborCounter > 3)) {
+        nextCellState = 0;
+      }
+      nextRow.push(nextCellState);
+    });
+    nextState.push(nextRow);
+  });
+  return nextState;
+}
+
+//var matrix =/*beacons*/ [[1, 1, 0, 0, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 0, 1, 1], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 1, 1, 0, 1, 1, 0, 0], [1, 1, 0, 0, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0, 0, 1, 1]];
+/*var matrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+              [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];*/
+
+
+
+
+var matrix = matrixCreator(25, 48);
 
 document.addEventListener("DOMContentLoaded", function(){
-  newCycle(matrix);
+  generateNewCycle(matrix);
+
+  document.querySelector("#tick").addEventListener('click', function(){
+    nextCycle();
+  });
+  function nextCycle(){
+    matrix = calculateNextState(matrix);
+    generateNewCycle(matrix);
+  }
+
+
+  setInterval(nextCycle, 250);
 });
 
 
